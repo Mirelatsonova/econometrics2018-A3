a212a757a1c0551758cca82f191fe5731ee82a6c
Boyko Amarov <boyko.amarov@gmail.com>
2018-05-04 11:20:32 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,18 +1,24 @@
 ## Problem set 3, Problem 1
 
 childiq <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/childiq.csv')
-
+str(childiq)
 ## a)
 ## Create two vectors with child IQ scores:
 ## - for children whose mother did not finish high school
-childIqNoHS <- 
+score0 <- childiq[???]
+## Size of group with mom_hs == 0
+n0 <- 
 
 ## - for children whose mother finished high school
-childIqWithHS <- 
+score1 <- childiq[???]
+## Size of group with mom_hs == 1
+n1 <- 
 
 ## Compute the test statistic for a two-samples t-test (equal variances)
+stdDevPooled <- 
 testStat <- 
-  
+
+t.test(x =???, y = ???, var.equal = TRUE)
 ## Compute the critical values at the 95% confidence level
 qt(???)
 qt(???)
@@ -36,6 +42,7 @@ lm(??? ~ ???, data = childiq)
 ## Estimated regression line: Y_hat = 
 
 ## c)
+
 
 
 
