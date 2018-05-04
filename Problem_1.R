5119d72d129d7670ccc44ea9334f75d6241a46c7
Boyko Amarov <boyko.amarov@gmail.com>
2018-05-04 11:07:33 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,12 +1,33 @@
 ## Problem set 3, Problem 1
 
-library(ggplot2)
-
 childiq <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/childiq.csv')
 
 ## a)
+## Create two vectors with child IQ scores:
+## - for children whose mother did not finish high school
+childIqNoHS <- 
 
-t.test(???, var.equal = TRUE)
+## - for children whose mother finished high school
+childIqWithHS <- 
+
+## Compute the test statistic for a two-samples t-test (equal variances)
+testStat <- 
+  
+## Compute the critical values at the 95% confidence level
+qt(???)
+qt(???)
+
+## Compare the test statistic and the critical values and 
+## decide whether to reject the null hypothesis or not.
+
+## Perform the test with the t.test function
+t.test(x = , y = , mu = , var.equal = TRUE)
+
+## Perform the test with the t.test function
+## using the formula interface
+
+
+## Check 
 
 ## b)
 
@@ -15,4 +36,6 @@ lm(??? ~ ???, data = childiq)
 ## Estimated regression line: Y_hat = 
 
 ## c)
+
+
 
